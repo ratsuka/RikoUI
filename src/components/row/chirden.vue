9619c6e9f6856bb9a6b07e7f60b5c787dcc25e73
fleurdoran <1256221681@qq.com>
2019-04-18 18:22:53 +0800

    删了不必要的东西

---
+++ src/components/row/chirden.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="'w'+width">
+     
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  components: {},
+  props:{
+      width:Number
+  }
+};
+</script>
+ <style scoped>
+</style>
---
+++ src/components/row/content.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content">
+    <slot></slot>
+  </div>
+</template>
+<script>
+import Line from './chirden.vue'
+export default {
+  data() {
+    return {};
+  },
+  components:{
+    Line
+  }
+};
+</script>
+ <style scoped>
+.content {
+  width: 100%;
+  min-height: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>
